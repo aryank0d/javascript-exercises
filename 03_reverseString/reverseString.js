188f4a36459bb99a3122d8f8184673acad056eb0
aryank0d <141023248+aryank0d@users.noreply.github.com>
2023-09-23 02:20:12 +0530

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(rev) {
+let string = '';
+  for(let i=rev.length();i>=0;i--)
+    {
+      string+=rev[i];
 };
 
 // Do not edit below this line
